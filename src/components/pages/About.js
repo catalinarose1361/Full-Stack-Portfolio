c0f9eeff4928126c509b6df5dd7a1594d7c6e196
Catalina Williams <catalina.rose1361@gmail.com>
2020-07-07 15:40:49 -0600

    added tabs to about me page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Tabs } from 'antd';
+const { TabPane } = Tabs;
 
 const About = () => (
   
@@ -45,7 +47,26 @@ const About = () => (
 
         </a>
 
-    </div>
+    </div> 
+    <div className="card-container">
+    <Tabs type="card">
+      <TabPane tab="Tab Title 1" key="1">
+        <p>Content of Tab Pane 1</p>
+        <p>Content of Tab Pane 1</p>
+        <p>Content of Tab Pane 1</p>
+      </TabPane>
+      <TabPane tab="Tab Title 2" key="2">
+        <p>Content of Tab Pane 2</p>
+        <p>Content of Tab Pane 2</p>
+        <p>Content of Tab Pane 2</p>
+      </TabPane>
+      <TabPane tab="Tab Title 3" key="3">
+        <p>Content of Tab Pane 3</p>
+        <p>Content of Tab Pane 3</p>
+        <p>Content of Tab Pane 3</p>
+      </TabPane>
+    </Tabs>
+  </div>
   </>
 );
 
