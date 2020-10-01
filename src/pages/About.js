fee2e9a8568763cac2ea20ec8b7c32ca6efc23d9
Catalina Williams <catalina.rose1361@gmail.com>
2020-10-01 13:37:34 -0600

    added icons to about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -51,16 +51,31 @@ const About = () => (
         </Row>
         <Row gutter={16}>
             <Col span={8}>
+                <Row justify={'center'}>
+
             <CrownTwoTone twoToneColor={'#b72c2c'} style={{ fontSize: '80px' }}  />
+                </Row>
+                <Row justify={'center'}>
             Elegant
+
+                </Row>
             </Col>
             <Col span={8}>
+            <Row justify={'center'}>
             <RocketTwoTone twoToneColor={'#b72c2c'} style={{ fontSize: '80px' }} />
+            </Row>
+            <Row justify={'center'}>
             Modern
+            </Row>
             </Col>
             <Col span={8}>
+            <Row justify={'center'}>
             <EyeTwoTone twoToneColor={'#b72c2c'} style={{ fontSize: '80px' }} />
+            </Row>
+            <Row justify={'center'}>
+
             Intuitive
+            </Row>
             </Col>
         </Row>
     
