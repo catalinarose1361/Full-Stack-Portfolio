654b3eb047a3d0ae99eacfe2ec894f0f7697f625
Catalina Williams <catalina.rose1361@gmail.com>
2021-01-23 13:40:17 -0700

    changed background color and icon for title page

--- src/pages/TitlePage.js
+++ src/pages/TitlePage.js
@@ -1,11 +1,11 @@
 import React from "react";
-import logo from '../images/Icons/logo_size.jpg';
+import logo from '../images/Icons/white_logo_transparent_background.png';
 
 const TitlePage = () => {
    
     return (
-      
-        <div id="background" style={{backgroundImage: `url(${logo})`, textAlign:'center'}}>
+      //backgroundImage: `url(${logo})`,
+        <div id="background" style={{backgroundColor:'#530EF0', backgroundImage: `url(${logo})`, textAlign:'center'}}>
          
         </div>
        
